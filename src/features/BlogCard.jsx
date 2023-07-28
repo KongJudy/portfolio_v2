a97de447f4c713433f4306cd7d8e9592d84a395b
Judy Kong <judy.kong817@gmail.com>
2023-07-28 19:05:54 -0400

    blog project title updated and link fixed

--- src/features/BlogCard.jsx
+++ src/features/BlogCard.jsx
@@ -1,5 +1,3 @@
-import AnchorLink from 'react-anchor-link-smooth-scroll';
-
 const BlogCard = () => {
   const image = require('../assets/blog-images/blog_ss01.png');
   const githubLink = 'https://github.com/KongJudy/react-connect-website';
@@ -7,18 +5,20 @@ const BlogCard = () => {
   return (
     <div className='bg-white rounded-lg shadow-lg overflow-hidden border-yellow border-2'>
       <img src={image} alt='blog site' className='w-full h-52 object-fit' />
-      <div className='p-2'>
-        <h2 className='text-xl font-bold'>Blog Website</h2>
-        <p className='text-deep-blue text-sm'>
+      <div className='p-2 text-center'>
+        <span className='text-deep-blue text-md font-bold'>Blog Website</span>
+        <p className='text-deep-blue text-sm mt-2'>
           Created using HTML, CSS, REACT, BOOTSTRAP
         </p>
         <div className='mt-4 flex justify-end'>
-          <AnchorLink
+          <a
             className='bg-gradient-rainbow border-2 border-deep-blue text-deep-blue rounded-md py-2 px-7 font-semibold hover:text-white transition duration-500'
             href={githubLink}
+            target='_blank'
+            rel='noreferrer'
           >
             <span className='tracking-wider'>Github</span>
-          </AnchorLink>
+          </a>
         </div>
       </div>
     </div>
