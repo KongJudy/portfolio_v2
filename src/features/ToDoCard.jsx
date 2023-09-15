e273ab7483b742091985643953dcac484ab669f8
Judy Kong <judy.kong817@gmail.com>
2023-09-15 14:37:45 -0400

    live demo added for todo app

--- src/features/ToDoCard.jsx
+++ src/features/ToDoCard.jsx
@@ -1,10 +1,10 @@
 const ToDoCard = () => {
   const image = require('../assets/todo-app.png');
-  const githubLink = 'https://github.com/KongJudy/mern-todo';
+  const demoLink = 'https://mern-todo-3d9h.onrender.com';
 
   return (
     <div className='bg-white rounded-lg shadow-lg overflow-hidden border-yellow border-2'>
-      <img src={image} alt='todo app' className='w-full h-52 object-fit' />
+      <img src={image} alt='todo app' className='w-full h-48 object-fit' />
       <div className='p-2 text-center'>
         <span className='text-deep-blue text-md font-bold'>Todo MERN app</span>
         <p className='text-deep-blue text-sm mt-2'>
@@ -14,11 +14,11 @@ const ToDoCard = () => {
         <div className='mt-4 flex justify-end'>
           <a
             className='bg-gradient-rainbow border-2 border-deep-blue text-deep-blue rounded-md py-2 px-7 font-semibold hover:text-white transition duration-500'
-            href={githubLink}
+            href={demoLink}
             target='_blank'
             rel='noreferrer'
           >
-            <span className='tracking-wider'>Github</span>
+            <span className='tracking-wider'>Live Demo</span>
           </a>
         </div>
       </div>
